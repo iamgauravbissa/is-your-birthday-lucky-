351b3d29afa52b8dc9e65bc0e3d03eb31ee77fa2
Gaurav Bissa <48169747+iamgauravbissa@users.noreply.github.com>
2020-12-25 21:00:32 +0530

    updated functionalities

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ var btn = document.getElementById('btn');
 btn.addEventListener('click', submitform);
 
 function submitform() {
+
     var date = document.getElementById('Date').value;
     var lucky = document.getElementById('lucky').value;
     var sum = getsum(date);
     if(sum%lucky==0)
     {
         youarelucky();
+
     }
     else{
         youarenotlucky();
@@ -27,9 +29,32 @@ function getsum(date) {
 }
 
 function youarelucky() {
-    console.log("your are a lucky")
+    document.getElementById('overlay').style.display="block";
+    document.getElementById('hideme').style.display ="none";
+    anime.timeline({loop: true})
+  .add({
+    targets: '.ml15 .word',
+    scale: [10,1],
+    opacity: [0,1],
+    easing: "easeOutCirc",
+    duration: 800,
+    delay: (el, i) => 800 * i
+  }).add({
+    targets: '.ml15',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+  window.setTimeout(relodefun , 5000);
 }
 
 function youarenotlucky() {
-    console.log("no you are not lucky")
+    document.getElementById('overlaynotlucky').style.display="block";
+    document.getElementById('hideme').style.display ="none";
+    window.setTimeout(relodefun , 3000);
+}
+
+function relodefun() {
+    location.reload();
 }
