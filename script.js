9b7f8664cab7df22bc67cec3c2a0e49cbe58f3f2
Gaurav Bissa <48169747+iamgauravbissa@users.noreply.github.com>
2020-12-25 19:40:25 +0530

    update index.html & added script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+var btn = document.getElementById('btn');
+btn.addEventListener('click', submitform);
+
+function submitform() {
+    var date = document.getElementById('Date').value;
+    var lucky = document.getElementById('lucky').value;
+    var sum = getsum(date);
+    if(sum%lucky==0)
+    {
+        youarelucky();
+    }
+    else{
+        youarenotlucky();
+    }
+}
+
+function getsum(date) {
+    var total = 0;
+    var dd = date;
+    for (i = 0; i < date.length; i++) {
+
+        total += dd % 10;
+
+        dd = Math.floor(dd / 10);
+    }
+    return total;
+}
+
+function youarelucky() {
+    
+}
+
+function youarenotlucky() {
+    
+}
